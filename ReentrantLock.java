97cb6188f73107075c75e86ce7e89cbaec24df4b
last-saiyan <50881789+last-saiyan@users.noreply.github.com>
2019-12-12 21:40:54 +0530

    Create ReentrantLock.java

---
+++ ReentrantLock.java
@@ -0,0 +1,29 @@
+public class Lock{
+
+  boolean isLocked = false;
+  Thread  lockedBy = null;
+  int     lockedCount = 0;
+
+  public synchronized void lock()
+  throws InterruptedException{
+    Thread callingThread = Thread.currentThread();
+    while(isLocked && lockedBy != callingThread){
+      wait();
+    }
+    isLocked = true;
+    lockedCount++;
+    lockedBy = callingThread;
+  }
+
+
+  public synchronized void unlock(){
+    if(Thread.curentThread() == this.lockedBy){
+      lockedCount--;
+
+      if(lockedCount == 0){
+        isLocked = false;
+        notify();
+      }
+    }
+  }
+}
