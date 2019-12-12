bd3cc57b35a7d5100140ad43e0e29bfb215fb83c
last-saiyan <50881789+last-saiyan@users.noreply.github.com>
2019-12-12 21:39:43 +0530

    Create simpleLock.java

---
+++ simpleLock.java
@@ -0,0 +1,17 @@
+public class Lock{
+
+  private boolean isLocked = false;
+
+  public synchronized void lock()
+  throws InterruptedException{
+    while(isLocked){
+      wait();
+    }
+    isLocked = true;
+  }
+
+  public synchronized void unlock(){
+    isLocked = false;
+    notify();
+  }
+}
