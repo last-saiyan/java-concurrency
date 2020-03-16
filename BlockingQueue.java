7f00069abed631243d25a57c0d8a6393b1a60ad9
nagesh srinivas <iam.nageshbs@gmail.com>
2020-03-16 23:03:34 +0530

    Create BlockingQueue.java

---
+++ BlockingQueue.java
@@ -0,0 +1,35 @@
+public class BlockingQueue {
+
+  private List queue = new LinkedList();
+  private int  limit = 10;
+
+  public BlockingQueue(int limit){
+    this.limit = limit;
+  }
+
+
+  public synchronized void enqueue(Object item)
+  throws InterruptedException  {
+    while(this.queue.size() == this.limit) {
+      wait();
+    }
+    this.queue.add(item);
+    if(this.queue.size() == 1) {
+      notifyAll();
+    }
+  }
+
+
+  public synchronized Object dequeue()
+  throws InterruptedException{
+    while(this.queue.size() == 0){
+      wait();
+    }
+    if(this.queue.size() == this.limit){
+      notifyAll();
+    }
+
+    return this.queue.remove(0);
+  }
+
+}
