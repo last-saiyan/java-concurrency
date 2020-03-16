ba9469b326deef641be936b55eda72ac684a5fbd
nagesh srinivas <iam.nageshbs@gmail.com>
2020-03-16 22:31:07 +0530

    Create threadpool.java

---
+++ threadpool.java
@@ -0,0 +1,30 @@
+public class Threadpool extends Thread {
+
+    private BlockingQueue taskQueue = null;
+    private boolean       isStopped = false;
+
+    public Threadpool(BlockingQueue queue){
+        taskQueue = queue;
+    }
+
+    public void run(){
+        while(!isStopped()){
+            try{
+                Runnable runnable = (Runnable) taskQueue.dequeue();
+                runnable.run();
+            } catch(Exception e){
+                //log or otherwise report exception,
+                //but keep pool thread alive.
+            }
+        }
+    }
+
+    public synchronized void doStop(){
+        isStopped = true;
+        this.interrupt(); //break pool thread out of dequeue() call.
+    }
+
+    public synchronized boolean isStopped(){
+        return isStopped;
+    }
+}
